a642de4806f3a3d93e0d91ffcacc107fc32ef8a5
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-04-20 13:12:39 -0500

    created model modules with base key-value pairs and imported the classes into the models.__init__.py

---
+++ models/demotion_queue.py
@@ -0,0 +1,5 @@
+class DemotionQueue():
+    def __init__(self, action, admin_id, approver_one_id):
+        self.action = action
+        self.admin_id = admin_id
+        self.approver_one_id = approver_one_id
