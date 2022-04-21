829fc85ac3fd00cf69a6318b7a0a9004a7ec2333
Henry House <hankhaus99@gmail.com>
2022-04-21 10:18:34 -0500

    comments for view tag list

---
+++ views/tag_requests.py
@@ -0,0 +1,6 @@
+# define a function to get all tags
+
+    # write sql block to select names/labels for all tags
+
+        # use ORDER BY to order them alphabetically
+        # eaxmple ORDER BY label ASC;
