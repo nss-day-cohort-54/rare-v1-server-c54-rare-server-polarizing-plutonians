2eedc24a50324c76fed24cee92f43869aa1f7cb8
David Clark <david.clark.1043@gmail.com>
2022-04-25 12:52:44 -0500

    Added subscribe functionality

--- views/subscription_requests.py
+++ views/subscription_requests.py
@@ -24,17 +24,31 @@ def get_all_subscriptions_by_user(user_id):
             # from subscriptions s
             # where s.follower_id = ?
             # ? tuple functionality interpolating user_id
-    
+        db_cursor.execute("""
+            SELECT
+                s.id,
+                s.follower_id,
+                s.author_id,
+                s.created_on
+            FROM Subscriptions s
+            WHERE s.follower_id = ?
+        """, (user_id, ))
+        
+        dataset = db_cursor.fetchall()
         # initialize empty list for subscriptions
         subscriptions = []
         # iterate over dataset from execute query
+        for row in dataset:
+            subscription = Subscription(row['id'], row['follower_id'],
+                                        row['author_id'], row['created_on'])
             # append the data as a Subscription object to the new list
-    
+            subscriptions.append(subscription.__dict__)
+            
     # return new list
     return json.dumps(subscriptions)
 
 
-def add_subscription(new_subscription):
+def create_subscription(new_subscription):
     """
     adds new subscription to the subscription table
 
@@ -55,13 +69,21 @@ def add_subscription(new_subscription):
             # subscription table
             # values follower_id, author_id, and created_on
             # interpolated using ? functionality
+        db_cursor.execute("""
+            INSERT INTO subscriptions
+                (follower_id, author_id, created_on)
+            VALUES
+                (?, ?, ?)
+        """, (new_subscription['followerId'],
+              new_subscription['authorId'],
+              new_subscription['createdOn']))
             
         # get id of new table row
-        
+        id = db_cursor.lastrowid
         # add id to the new_subscription object
-        
+        new_subscription['id'] = id
     # return the new_subscription object with the id as added
-    return ""
+    return json.dumps(new_subscription)
 
 def delete_subscription(subscription_id):
     """
@@ -80,6 +102,9 @@ def delete_subscription(subscription_id):
         # delete from sql statement
             # subscriptions
             # where s.id = subscription_id # interpolate using ? functionality
+        db_cursor.execute("""
+            DELETE FROM Subscriptions
+            WHERE id = ?    
+        """, (subscription_id, ))
     
     # doesn't need to return anything
-    return ""
