e5b9f444a67cf5b378483b54ccd80b8de6d5b378
Henry House <hankhaus99@gmail.com>
2022-04-27 13:31:14 -0500

    filter by tag working

--- views/__init__.py
+++ views/__init__.py
@@ -3,7 +3,7 @@
 from .post_requests import get_all_posts
 from .post_requests import get_single_post
 from .post_requests import create_post
-from .post_requests import edit_post
+from .post_requests import edit_post, get_posts_by_tag
 from .post_requests import delete_post
 from .post_requests import get_posts_by_title
 from .post_requests import get_posts_by_user_id
